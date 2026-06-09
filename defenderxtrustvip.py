a0cc1720602a9217c69f089d46258376df9571f8
godk9630 <godk9630@gmail.com>
2026-06-09 11:16:18 +0530

    Update defenderxtrustvip.py

--- defenderxtrustvip.py
+++ defenderxtrustvip.py
@@ -20,7 +20,7 @@ redeem_codes_collection = db['redeem_codes0']
 
 # Bot Configuration
 TELEGRAM_BOT_TOKEN = '8780997472:AAHyfDg02OBPrpO1wjYHq93iuuYCw4ROgzU'
-ADMIN_USER_ID = 8362081186  # Replace with your admin user ID
+ADMIN_USER_ID = '2077682354'  # Replace with your admin user ID
 
 async def help_command(update: Update, context: CallbackContext):
     user_id = update.effective_user.id
